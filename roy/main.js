0b326769506d30f3c78e70635b1029986a7d328e
Samuel Gélineau <gelisam@gmail.com>
2012-12-08 12:34:49 -0500

    Add more code/value pairs, closes #1

--- roy/main.js
+++ roy/main.js
@@ -4,9 +4,17 @@ var mkPair = function(text) {
     };
 };
 var mkApp = function() {
+    var pairs = ko.observableArray([mkPair("Hello"), mkPair("World")]);
     return {
-        "pairs": ko.observableArray([mkPair("Hello"), mkPair("World")])
+        "pairs": pairs,
+        "new_expr": ko.observable(("")),
+        "new_pair": function() {
+            var expr = value(this.new_expr);
+            var pair = mkPair(expr);
+            pairs.push(pair);
+            return this.new_expr("");
+        }
     };
 };
-ko.applyBindings(mkApp());
+ko.applyBindings(value(mkApp));
 //@ sourceMappingURL=main.js.map
